de8f3fb498eff2e944d710d3aca6004c4509a9f3
axexlck <axelclk@gmail.com>
2016-07-27 18:19:04 +0200

    Use Java8 JAR

--- symja_scala_examples/src/org/matheclipse/scala/Calculus.scala
+++ symja_scala_examples/src/org/matheclipse/scala/Calculus.scala
@@ -8,19 +8,19 @@ import org.matheclipse.parser.client.math.MathException
 object Calculus {
 
   def main(args: Array[String]) {
-    Config.PARSER_USE_LOWERCASE_SYMBOLS = true;
+    //    Config.PARSER_USE_LOWERCASE_SYMBOLS = true;
     try {
       val util = new ExprEvaluator(false, 100);
       val scalaForm = util.toScalaForm("D(sin(x)*cos(x),x)");
       println(scalaForm);
       val result = util.evaluate(D(Sin(x) * Cos(x), x));
       println(result);
-      
+
       val scalaForm2 = util.toScalaForm("Integrate(sin(x)^5,x)");
       println(scalaForm2);
-      val result2 = util.evaluate(Integrate(Power(Sin(x),5),x));
+      val result2 = util.evaluate(Integrate(Power(Sin(x), 5), x));
       println(result2);
-      
+
     } catch {
       case e: Exception => println("exception caught: " + e);
     }
