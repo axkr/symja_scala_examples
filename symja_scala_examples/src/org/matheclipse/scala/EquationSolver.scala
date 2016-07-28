d41fe63a41ad017c6422a82d5df28d06e9a4c9eb
axexlck <axelclk@gmail.com>
2016-07-28 17:51:23 +0200

    Added EquationSolver example

---
+++ symja_scala_examples/src/org/matheclipse/scala/EquationSolver.scala
@@ -0,0 +1,25 @@
+package org.matheclipse.scala
+
+import org.matheclipse.core.eval.ExprEvaluator
+import org.matheclipse.core.expression.F._
+
+object Solver {
+
+  def main(args: Array[String]) {
+    try {
+      // use the static methods in org.matheclipse.core.expression.F
+      val result = eval(Solve(List(Equal(Sqr(x) + ZZ(11L), y), Equal(x + y, CN9)), List(x,y)));
+      println(result);
+      
+      // generate scala input from parsed string:
+      val util = new ExprEvaluator(false, 100);
+      val scalaForm = util.toScalaForm("Solve({x^2+11==y, x+y==-9}, {x,y})");
+      println("Scala input form: "+scalaForm);
+      
+      
+ 
+    } catch {
+      case e: Exception => println("exception caught: " + e);
+    }
+  }
+}
